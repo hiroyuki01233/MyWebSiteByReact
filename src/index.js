78dcb565660ec9cdc67986636fcb5ecf9cdcd3e9
hiroyuki01123 <hiroyuki_19n1100144@nnn.ed.jp>
2019-09-06 15:15:21 +0900

    first

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let i = 0
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+const Main = props =>　{
+
+    function random(){
+        var color = ["blue","red","aqua","chartreuse","deeppink"]
+        var hello = document.getElementById("hello")
+        hello.style.backgroundColor = color[Math.floor( Math.random() * color.length )]
+    }
+
+    function setcolor(){
+        var whatcolor = ["blue","red","aqua","chartreuse","deeppink"]
+        var change = document.getElementById("result")
+        change.style.backgroundColor = whatcolor[Math.floor( Math.random() * whatcolor.length )]
+    }
+
+    function changeColor(){
+        setInterval(setcolor, 100);
+    }
+
+
+    let or = 1
+    
+    const Btn = () => {
+        i = i + 1
+        console.log(i + "本")
+        document.getElementById("result").innerHTML = i + "本だよ！"
+        random()
+        or = or + 1
+        if (or == 2){
+            changeColor()
+        }
+         
+    }
+
+
+
+    return(
+        <div>
+        <h1 id="hello">Hello World</h1>
+        <button onClick={Btn}>何本？</button>
+        <h1 id="result"></h1>
+        </div>
+    )
+
+}
+
+
+render(
+    <Main/>,
+    document.getElementById("root")
+)
