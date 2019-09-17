c82d908876313f7855abd86b382c50ce6deabe56
hiroyuki01123 <hiroyuki_19n1100144@nnn.ed.jp>
2019-09-17 13:24:42 +0900

    btn

--- src/index.js
+++ src/index.js
@@ -2,49 +2,44 @@ import React from 'react';
 import { render } from 'react-dom';
 import "./index.css"
 
-let i = 0
-
-
 
 const Main = props =>　{
 
-    function random(){
-        var color = ["blue","red","aqua","chartreuse","deeppink"]
-        var hello = document.getElementById("hello")
-        hello.style.backgroundColor = color[Math.floor( Math.random() * color.length )]
-    }
+    // function random(){
+    //     var color = ["blue","red","aqua","chartreuse","deeppink"]
+    //     var hello = document.getElementById("hello")
+    //     hello.style.backgroundColor = color[Math.floor( Math.random() * color.length )]
+    // }
 
-    function setcolor(){
-        var whatcolor = ["blue","red","aqua","chartreuse","deeppink"]
-        var change = document.getElementById("result")
-        change.style.backgroundColor = whatcolor[Math.floor( Math.random() * whatcolor.length )]
-    }
+    // function setcolor(){
+    //     var whatcolor = ["blue","red","aqua","chartreuse","deeppink"]
+    //     var change = document.getElementById("result")
+    //     change.style.backgroundColor = whatcolor[Math.floor( Math.random() * whatcolor.length )]
+    // }
 
-    function changeColor(){
-        setInterval(setcolor, 100);
-    }
-
-
-    let or = 1
+    // function changeColor(){
+    //     setInterval(setcolor, 100);
+    // }
     
-    const Btn = () => {
-        i = i + 1
-        console.log(i + "本")
-        document.getElementById("result").innerHTML = i + "本だよ！"
-        random()
-        or = or + 1
-        if (or == 2){
-            changeColor()
-        }
-         
+    const homebtn = () => {
+        var greet = document.getElementById("hello")
+        greet.innerHTML = 'こんにちは～～'        
     }
-
-
+    const aboutbtn = () => {
+        var greet = document.getElementById("hello")
+        greet.innerHTML = 'about'        
+    }
+    const workbtn = () => {
+        var greet = document.getElementById("hello")
+        greet.innerHTML = 'works'         
+    }
 
     return(
         <div>
         <h1 id="hello">Hello World</h1>
-        <button onClick={Btn}>何本？</button>
+        <button onClick={homebtn}>Home</button>
+        <button onClick={aboutbtn}>About</button>
+        <button onClick={workbtn}>Works</button>
         <h1 id="result"></h1>
         </div>
     )
